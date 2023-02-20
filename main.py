ad41e3a9afcca39d173f434970edd2852046a3fe
sreejith <squeeky.mouse@gmail.com>
2023-02-20 20:36:13 +0530

    updated port and url

--- main.py
+++ main.py
@@ -1,26 +1,25 @@
 from flask import Flask,render_template
 import pandas as pd
-#commit: df search api implemented Sec29
-# commit: html updated Sec29
+#commit: updated port and url Sec29
 
 app=Flask(__name__)
 
-df=pd.read_csv('dictionary.csv')
+df=pd.read_csv('dictionary.csv') #load csv in df
 
 @app.route('/')
 def home():
     return(render_template('index.html'))
 
-@app.route('/api/vi/<word>')
+@app.route('/api/vi/<word>/')
 def translate(word):
     definition=''
 
-    def_df=df.query(f'word=="{word}"')
-    for index,row in def_df[0:].iterrows():
+    def_df=df.query(f'word=="{word}"') #query df and get result df
+    for index,row in def_df.iterrows(): #get definitions fron the df
         definition=f"{definition}{row['definition']}\n"
-
-    return({'definition':definition,
-            'word':word})
+    
+    result_dict={'definition':definition,'word':word} #form dict/json to return
+    return(result_dict)
 
 if __name__=='__main__':
-    app.run(debug=True)
+    app.run(debug=True,port=5001)
