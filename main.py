0bfe6375daadfcfc97650d1005e9469ce5656fd3
sreejith <squeeky.mouse@gmail.com>
2023-02-23 19:01:17 +0530

    add app fn using loc,series and df,query Sec30

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask,render_template
 import pandas as pd
-#commit: updated port and url Sec29
+#commit: add app fn using loc,series and df,query Sec30
 
 app=Flask(__name__)
 
@@ -10,14 +10,27 @@ df=pd.read_csv('dictionary.csv') #load csv in df
 def home():
     return(render_template('index.html'))
 
-@app.route('/api/vi/<word>/')
-def translate(word):
-    definition=''
+@app.route('/api/v1/<word>/')
+def api(word):
+    # this uses the df.loc!!!
+    def_series=df.loc[df['word']==word]['definition'] #this returns a series
+    if def_series.empty: #check if the series is empty
+        definition=''
+    else:
+        definition=def_series.squeeze()
+    result_dict={'definition':definition,'word':word} #form dict/json to return
+    return(result_dict)
 
-    def_df=df.query(f'word=="{word}"') #query df and get result df
-    for index,row in def_df.iterrows(): #get definitions fron the df
-        definition=f"{definition}{row['definition']}\n"
-    
+@app.route('/api/v2/<word>/')
+def translate(word):
+    # this uses the df.query() method!!!
+    def_df=df.query(f'word=="{word}"') #query df and returns a df!!!
+    # df.query already returns all results concatenated, so no need to iterate!!!
+    # def_df.isnull() #this checks for empty and puts bool in each cell is DF!!!
+    if def_df['definition'].empty: # check if cell is empty in the df!!!
+        definition=''
+    else:
+        definition=f"{def_df['definition'].squeeze()}"
     result_dict={'definition':definition,'word':word} #form dict/json to return
     return(result_dict)
 
